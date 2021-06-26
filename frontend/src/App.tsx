1be5a2590514d8e06fca48b4a0217fe09431f94d
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-06-27 00:27:21 +0900

    removed sidebar and put contents into navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import Sidebar from './components/Sidebar/Sidebar';
 
 const App = () => {
   return (
     <React.Fragment>
       <Navbar/>
-      <Sidebar/>
     </React.Fragment>
   )
 }
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -7,24 +7,40 @@ const Navbar = () => {
   return (
     <React.Fragment>
       <nav className="navbar" role="navigation" aria-label="main navigation">
-        <NavbarBrand/>
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-end">
-              <div className="buttons">
-                <a className="button is-light">Log in</a>
-              </div>
-          </div>
-        </div>
-      </nav>
-      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <NavbarBrand />
         <div id="navbarBasicExample" className="navbar-menu">
           <div className="navbar-start">
             <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">Model Lists</a>
+              <a className="navbar-link">Image Classification</a>
               <div className="navbar-dropdown">
-                <a className="navbar-item" href="models/human-pose">Human Pose Detection</a>
+                <a className="navbar-item" href="models/human-pose">
+                  ic
+                </a>
                 {/* <hr className="navbar-divider"></hr> */}
               </div>
+            </div>
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link">Object Detection</a>
+              <div className="navbar-dropdown">
+                <a className="navbar-item" href="models/human-pose">
+                  od
+                </a>
+                {/* <hr className="navbar-divider"></hr> */}
+              </div>
+            </div>
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link">Other Models</a>
+              <div className="navbar-dropdown">
+                <a className="navbar-item" href="models/human-pose">
+                  Human Pose Detection
+                </a>
+                {/* <hr className="navbar-divider"></hr> */}
+              </div>
+            </div>
+          </div>
+          <div className="navbar-end">
+            <div className="buttons">
+              <a className="button is-light">Log in</a>
             </div>
           </div>
         </div>
